96325f8b9a31adffbf2a9ba3753f925f2d93465f
Romain Fallet <rfallet@numee.fr>
2020-04-07 11:58:28 +0200

    Add mongoose connection.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+require('dotenv-flow').config()
+const mongoose = require('mongoose')
 const app = require('./app')
 
-// Run the server!
 const start = async () => {
   try {
+    // Connect to the database
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    // Start the webserver
     await app.listen(3000)
+
     app.log.info(`server listening on ${app.server.address().port}`)
   } catch (err) {
     app.log.error(err)
