91ba29a63ce96829e8942b116f4169173ecd3e12
Romain Fallet <rfallet@numee.fr>
2020-04-16 16:06:32 +0200

    Add timestamps.

--- src/models/cat.js
+++ src/models/cat.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 
-const schema = new mongoose.Schema({
-  name: String,
-  color: String
-})
+const schema = new mongoose.Schema(
+  {
+    name: String,
+    color: String
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Cat', schema)
