ad7822b1356d90adccf3c6cbf653648c2e3f0cca
Romain Fallet <rfallet@numee.fr>
2020-04-16 16:07:07 +0200

    Improve fixture + make it importable in tests.

--- fixtures/cat.js
+++ fixtures/cat.js
@@ -1,6 +1,22 @@
 /* eslint-env mongo */
-db.cats.remove({})
-db.cats.insert({
+// eslint-disable-next-line no-global-assign
+ObjectId =
+  typeof ObjectId !== 'undefined'
+    ? ObjectId
+    : require('mongoose').Types.ObjectId
+
+const cat = {
+  _id: ObjectId('5e980b9bafdcc9eda8df1ffc'),
+  __v: 0,
   name: 'Kitty',
-  color: 'brown'
-})
+  color: 'brown',
+  createdAt: new Date('2020-04-16T08:57:33.198Z'),
+  updatedAt: new Date('2020-04-16T08:57:33.198Z')
+}
+
+if (typeof db !== 'undefined') {
+  db.cats.remove({})
+  db.cats.insert(cat)
+}
+
+module.exports = cat
